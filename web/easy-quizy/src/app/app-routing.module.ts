bee02ab31d557597561749077c0dc874eb020469
KarolJaskolka <jaskolka.karol@gmail.com>
2020-03-13 11:17:16 +0100

    add routing

--- web/easy-quizy/src/app/app-routing.module.ts
+++ web/easy-quizy/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainContentComponent } from './Components/main-content/main-content.component';
-
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { AccountMainComponent } from './Components/account/account-main/account-main.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch:'full'},
-  {path: 'home', component: MainContentComponent}
+  {path: 'home', component: MainContentComponent},
+  {path: 'signUp', component: SignUpComponent},
+  {path: 'account', component: AccountMainComponent}
 ];
 
 @NgModule({
--- web/easy-quizy/src/app/app.module.ts
+++ web/easy-quizy/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { TopNavbarComponent } from './Components/top-navbar/top-navbar.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { MainContentComponent } from './Components/main-content/main-content.component';
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { AccountMainComponent } from './Components/account/account-main/account-main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavbarComponent,
     FooterComponent,
-    MainContentComponent
+    MainContentComponent,
+    SignUpComponent,
+    AccountMainComponent
   ],
   imports: [
     BrowserModule,
