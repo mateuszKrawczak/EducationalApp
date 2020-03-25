b790c2ab3884a65f16bfe95e40f66f4a80fb09a6
mateuszKrawczak <241318@student.pwr.edu.pl>
2020-03-25 14:41:07 +0100

    account created with editing user data

--- web/easy-quizy/src/app/Components/account/account-main/account-main.component.ts
+++ web/easy-quizy/src/app/Components/account/account-main/account-main.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { StorageService } from 'src/app/Services/storageService';
+import { UserService } from 'src/app/Services/userService';
+import { User } from 'src/app/Models/user';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-account-main',
   templateUrl: './account-main.component.html',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountMainComponent implements OnInit {
 
-  constructor() { }
+  public userForm :FormGroup;
+  user:User ;
+  private id;
+  constructor(private toastrService:ToastrService,private storageService : StorageService, private userService:UserService, private fb:FormBuilder) {
+    
+    
+   }
 
   ngOnInit(): void {
+    
+    this.getUserData();
+    
   }
 
+  getUserData(){
+    this.id = this.storageService.getUserId();
+    this.userService.getUser(this.id).subscribe((currentUser) => {
+      this.user = currentUser;
+      //console.log(currentUser);
+      this.updateForm();
+    });
+    
+    
+  }
+  
+  updateForm(){
+    this.userForm = this.fb.group({
+      first_name: [this.user.first_name, Validators.required],
+      last_name: [this.user.last_name, Validators.required],
+      login: [this.user.login, Validators.required],
+      password: [this.user.password, Validators.required],
+      email: [this.user.email, [Validators.required,Validators.email]]
+    });
+  }
+
+  public changeUserData(){
+   
+    if(this.userForm.valid){
+        this.userService.changeUserData(this.userForm.value,this.id).subscribe((editedUser)=>{
+          this.user = editedUser;
+          //this.updateForm();
+          this.toastrService.success('Konto zostało zaktualizowane', 'Miłej gry!');
+    }, error => {
+      this.toastrService.success('Błąd');
+    })
+    }
+
+  }
 }
--- web/easy-quizy/src/app/Services/userService.ts
+++ web/easy-quizy/src/app/Services/userService.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import{User} from '../Models/user';
 @Injectable({
     providedIn: 'root'
 })
@@ -9,4 +11,14 @@ export class UserService {
 
     constructor(private http: HttpClient) {}
 
+    host:string = environment.host;
+
+    getUser(id):Observable<User>{
+        return this.http.get<User>(this.host+"api/users/"+id);
+    }
+
+    changeUserData(user:User,id):Observable<User>{
+        return this.http.put<User>(this.host+"api/users/"+id, user);
+
+    }
 }
